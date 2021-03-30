f6941cc37058a6add11c56fee06df5ad11ad983f
Selim <selimchemutai19@gmail.com>
2021-03-30 11:29:42 +0300

    second commit

--- app/src/main/java/com/pamela/tictactoe/MainActivity.kt
+++ app/src/main/java/com/pamela/tictactoe/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.pamela.tictactoe
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,40 +14,99 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun buClick(view: View){
-        val buSelected=view as Button
-        var cellID=0
-        when(buSelected.id){
-            R.id.bt1->cellID=1
-            R.id.bt2->cellID=2
-            R.id.bt3->cellID=3
-            R.id.bt4->cellID=4
-            R.id.bt5->cellID=5
-            R.id.bt6->cellID=6
-            R.id.bt7->cellID=7
-            R.id.bt8->cellID=8
-            R.id.bt9->cellID=9
+
+    fun buClick(view: View) {
+        val buSelected = view as Button
+        var cellID = 0
+        when (buSelected.id) {
+            R.id.bt1 -> cellID = 1
+            R.id.bt2 -> cellID = 2
+            R.id.bt3 -> cellID = 3
+            R.id.bt4 -> cellID = 4
+            R.id.bt5 -> cellID = 5
+            R.id.bt6 -> cellID = 6
+            R.id.bt7 -> cellID = 7
+            R.id.bt8 -> cellID = 8
+            R.id.bt9 -> cellID = 9
         }
-        Toast.makeText(this,"cell id"+cellID,Toast.LENGTH_LONG).show()
-        playGame(cellID,buSelected)
+        Toast.makeText(this, "cell id" + cellID, Toast.LENGTH_LONG).show()
+        playGame(cellID, buSelected)
     }
 
-    private fun playGame(cellID: Int, buSelected: Button) {
-        var player1=ArrayList<Int>()
-        var player2=ArrayList<Int>()
-        var activeplayer=1
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+    var activeplayer = 1
 
-        if (activeplayer==1){
-            buSelected.text="X"
+    private fun playGame(cellID: Int, buSelected: Button) {
+
+
+        if (activeplayer == 1) {
+            buSelected.text = "X"
+            buSelected.setBackgroundColor(Color.parseColor("#2B2B2B"))
             player1.add(cellID)
-            activeplayer=2
-        }
-        else {
+            activeplayer = 2
+        } else {
             buSelected.text = "0"
+            buSelected.setBackgroundColor(Color.parseColor("#2B2B2B"))
             player2.add(cellID)
             activeplayer = 1
         }
-        buSelected.isEnabled=false
+        buSelected.isEnabled = false
+        checkWinner()
     }
 
+    private fun checkWinner() {
+        var winner = -1
+
+//row
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+            winner = 1
+        }
+        if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+            winner = 2
+        }
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+            winner = 1
+        }
+        if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+            winner = 2
+        }
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+            winner = 1
+        }
+        if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+            winner = 1
+        }
+        //column
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+            winner = 1
+        }
+        if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+            winner = 2
+        }
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+            winner = 1
+        }
+        if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+            winner = 2
+        }
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+            winner = 1
+        }
+        if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+            winner = 1
+        }
+
+        //announcing the winner
+        if (winner==-1){
+            if (winner==1) {
+                Toast.makeText(this, "player 1 won the game", Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this, "player 2 won the game", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 }
+
+
